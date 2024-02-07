5a44ab7d6f1b8ae4135611c917fe25b42f28bd19
newbee1939 <hideto3970@gmail.com>
2024-02-07 09:05:47 +0900

    Slack投稿に関する実装を追加

--- app.py
+++ app.py
@@ -1,8 +1,83 @@
 # 質問に関連した文書をPineconeから検索して応答する
+import json
+import logging
+import os
+import re
+import time
+from datetime import timedelta
+from typing import Any
+
 from add_document import initialize_vectorstore
-from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory, MomentoChatMessageHistory
+from langchain.schema import LLMResult
+from slack_bolt import App
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+CHAT_UPDATE_INTERVAL_SEC = 1
+
+load_dotenv()
+
+# ログ
+SlackRequestHandler.clear_all_log_handlers()
+logging.basicConfig(
+    format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+# ボットトークンを使ってアプリを初期化します
+app = App(
+    signing_secret=os.environ["SLACK_SIGNING_SECRET"],
+    token=os.environ["SLACK_BOT_TOKEN"],
+    process_before_response=True,
+)
+
+class SlackStreamingCallbackHandler(BaseCallbackHandler):
+    last_send_time = time.time()
+    message = ""
+
+    def __init__(self, channel, ts):
+        self.channel = channel
+        self.ts = ts
+        self.interval = CHAT_UPDATE_INTERVAL_SEC
+        # 投稿を更新した累計回数カウンタ
+        self.update_count = 0
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.message += token
+
+        now = time.time()
+        if now - self.last_send_time > self.interval:
+            app.client.chat_update(
+                channel=self.channel, ts=self.ts, text=f"{self.message}\n\nTyping..."
+            )
+            self.last_send_time = now
+            self.update_count += 1
+
+            # update_countが現在の更新間隔X10より多くなるたびに更新間隔を2倍にする
+            if self.update_count / 10 > self.interval:
+                self.interval = self.interval * 2
+
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
+        message_context = "OpenAI APIで生成される情報は不正確または不適切な場合がありますが、当社の見解を述べるものではありません。"
+        message_blocks = [
+            {"type": "section", "text": {"type": "mrkdwn", "text": self.message}},
+            {"type": "divider"},
+            {
+                "type": "context",
+                "elements": [{"type": "mrkdwn", "text": message_context}],
+            },
+        ]
+        app.client.chat_update(
+            channel=self.channel,
+            ts=self.ts,
+            text=self.message,
+            blocks=message_blocks,
+        )
 
 def handle_mention(event, say):
     channel = event["channel"]
@@ -16,7 +91,7 @@ def handle_mention(event, say):
         result = say("\n^nTyping...", thread_ts=thread_ts)
         ts = result["ts"]
 
-        history = MomentoChaMessageHistory.from_client_params(
+        history = MomentoChatMessageHistory.from_client_params(
             id_ts,
             os.environ["MOMENTO_CACHE"],
             timedelta(hours=int(os.environ["MOMENTO_TTL"])),
