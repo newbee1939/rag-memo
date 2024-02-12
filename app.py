6ceb3e730b01c0387307cba46e76746379730a36
newbee1939 <hideto3970@gmail.com>
2024-02-12 16:08:51 +0900

    不要なコメントを削除した

--- app.py
+++ app.py
@@ -1,41 +1,24 @@
 # 質問に関連した文書をPineconeから検索して応答する
 import os
+import re
+import time
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-
-# from slack_bolt.adapter.aws_lambda import SlackRequestHandler
-
-# import json
-# import logging
-import re
-import time
 from typing import Any
-# from datetime import timedelta
-
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import LLMResult
-# from add_document import initialize_vectorstore
-# from langchain.chains import ConversationalRetrievalChain
-# from langchain.memory import ConversationBufferMemory, MomentoChatMessageHistory
 
 CHAT_UPDATE_INTERVAL_SEC = 1
 
 load_dotenv()
 
-# ログ
-# SlackRequestHandler.clear_all_log_handlers()
-# logging.basicConfig(
-#     format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO
-# )
-# logger = logging.getLogger(__name__)
-
-# ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
+# NOTE: Botトークンとソケットモードハンドラーを使ってアプリを初期化する。
 # AWS Lambdaで実行することを想定し、リスナー関数での処理が完了するまでHTTPレスポンスの送信を遅延させる。
 # AWS LambdaのようなFunction as ServiceではHTTPレスポンスを返した後にスレッドやプロセスの実行を続けることができないため、
 # FaaSで応答を別インスタンスで実行可能にする
-# FaaSで起動する場合process_before_response=Trueは必須の設定
+# FaaSで起動する場合、process_before_response=Trueは必須の設定となる
 # 参考: https://slack.dev/bolt-python/ja-jp/concepts
 app = App(
     signing_secret=os.environ["SLACK_SIGNING_SECRET"],
@@ -51,43 +34,22 @@ class SlackStreamingCallbackHandler(BaseCallbackHandler):
     def __init__(self, channel, ts):
         self.channel = channel
         self.ts = ts
-        # self.interval = CHAT_UPDATE_INTERVAL_SEC
-        # 投稿を更新した累計回数カウンタ
-        # self.update_count = 0
 
     def on_llm_new_token(self, token: str, **kwargs) -> None:
         self.message += token
 
         now = time.time()
-        # if now - self.last_send_time > self.interval:
         if now - self.last_send_time > CHAT_UPDATE_INTERVAL_SEC:
             self.last_send_time = now
             app.client.chat_update(
-                # channel=self.channel, ts=self.ts, text=f"{self.message}\n\nTyping..."
                 channel=self.channel, ts=self.ts, text=f"{self.message}..."
             )
-            # self.last_send_time = now
-            # self.update_count += 1
-
-            # update_countが現在の更新間隔X10より多くなるたびに更新間隔を2倍にする
-            # if self.update_count / 10 > self.interval:
-            #     self.interval = self.interval * 2
 
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
-        # message_context = "OpenAI APIで生成される情報は不正確または不適切な場合がありますが、当社の見解を述べるものではありません。"
-        # message_blocks = [
-        #     {"type": "section", "text": {"type": "mrkdwn", "text": self.message}},
-        #     {"type": "divider"},
-        #     {
-        #         "type": "context",
-        #         "elements": [{"type": "mrkdwn", "text": message_context}],
-        #     },
-        # ]
         app.client.chat_update(
             channel=self.channel,
             ts=self.ts,
             text=self.message
-            # blocks=message_blocks,
         )
 
 @app.event("app_mention")
@@ -107,58 +69,8 @@ def handle_mention(event, say):
         callbacks=[callback] 
     )
 
-    # response = llm.predict(message)
     llm.predict(message)
 
-    # ts=timestamp
-    # thread_ts = event["ts"]
-
-    # say(text=response, thread_ts=thread_ts)
 
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-
-# def handle_mention(event, say):
-#     channel = event["channel"]
-#     thread_ts = event["ts"]
-#     message = re.sub("<@.*", "", event["text"])
-
-#     # 投稿の先頭（=Momentoキー）を示す: 初回はevent["ts"], 2回目以降はevent["thread_ts"]
-#     id_ts = event["ts"]
-#     if "thread_ts" in event:
-#         id_ts = event["tread_ts"]
-#         result = say("\n^nTyping...", thread_ts=thread_ts)
-#         ts = result["ts"]
-
-#         history = MomentoChatMessageHistory.from_client_params(
-#             id_ts,
-#             os.environ["MOMENTO_CACHE"],
-#             timedelta(hours=int(os.environ["MOMENTO_TTL"])),
-#         ) 
-#         memory = ConversationBufferMemory(
-#             chat_memory=history, memory_key="chat_history", return_messages=True
-#         )
-
-#         vectorstore = initialize_vectorstore()
-
-#         callback = SlackStreamingCallbackHandler(channel=channel, ts=ts)
-#         llm = ChatOpenAI(
-#             model_name=os.environ["OPENAI_API_MODEL"],
-#             temperature=os.environ["OPENAI_API_TEMPERATURE"],
-#             streaming=True,
-#             callbacks=[callback]
-#         )
-
-#         condense_question_llm = ChatOpenAI(
-#             model_name=os.environ["OPENAI_API_MODEL"],
-#             temperature=os.environ["OPENAI_API_TEMPERATURE"],
-#         )
-
-#         qa_chain = ConversationalRetrievalChain.from_llm(
-#             llm=llm,
-#             retriever=vectorstore.as_retriever(),
-#             memory=memory,
-#             condense_question_llm=condense_question_llm,
-#         )
-
-#         qa_chain.run(message)
