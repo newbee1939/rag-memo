49c2c8a0cbb3eb9fc07165bcfe590f32bfbfb156
newbee1939 <hideto3970@gmail.com>
2024-02-12 16:00:22 +0900

    Implement streaming updates for Slack bot responses
    Enhanced the Slack bot to support streaming updates by introducing the
    SlackStreamingCallbackHandler class, which dynamically updates messages
    as new content is generated. Previously, responses were sent as a single
    message after processing was complete, which could lead to delays in
    interaction. This change aims to provide a more interactive and engaging
    user experience by showing real-time feedback ("...typing") as the
    response is being formulated, utilizing the chat_update API for
    incremental updates. This update aligns with improving responsiveness
    and transparency in bot communications, making conversations feel more
    natural and engaging.
    
    Refined response handling also streamlines the interaction flow and
    ensures users are kept informed of the bot's processing status,
    minimizing confusion and enhancing user engagement during longer
    processing times.

--- app.py
+++ app.py
@@ -43,67 +43,80 @@ app = App(
     process_before_response=True,
 )
 
+# 応答ストリームを受け取るCallbackハンドラークラス
+class SlackStreamingCallbackHandler(BaseCallbackHandler):
+    last_send_time = time.time()
+    message = ""
+
+    def __init__(self, channel, ts):
+        self.channel = channel
+        self.ts = ts
+        # self.interval = CHAT_UPDATE_INTERVAL_SEC
+        # 投稿を更新した累計回数カウンタ
+        # self.update_count = 0
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.message += token
+
+        now = time.time()
+        # if now - self.last_send_time > self.interval:
+        if now - self.last_send_time > CHAT_UPDATE_INTERVAL_SEC:
+            self.last_send_time = now
+            app.client.chat_update(
+                # channel=self.channel, ts=self.ts, text=f"{self.message}\n\nTyping..."
+                channel=self.channel, ts=self.ts, text=f"{self.message}..."
+            )
+            # self.last_send_time = now
+            # self.update_count += 1
+
+            # update_countが現在の更新間隔X10より多くなるたびに更新間隔を2倍にする
+            # if self.update_count / 10 > self.interval:
+            #     self.interval = self.interval * 2
+
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
+        # message_context = "OpenAI APIで生成される情報は不正確または不適切な場合がありますが、当社の見解を述べるものではありません。"
+        # message_blocks = [
+        #     {"type": "section", "text": {"type": "mrkdwn", "text": self.message}},
+        #     {"type": "divider"},
+        #     {
+        #         "type": "context",
+        #         "elements": [{"type": "mrkdwn", "text": message_context}],
+        #     },
+        # ]
+        app.client.chat_update(
+            channel=self.channel,
+            ts=self.ts,
+            text=self.message
+            # blocks=message_blocks,
+        )
+
 @app.event("app_mention")
 def handle_mention(event, say):
+    channel = event["channel"]
+    thread_ts = event["ts"]
     message = re.sub("<@.*>", "", event["text"])
 
+    result = say("\n\nTyping...", thread_ts=thread_ts)
+    ts = result["ts"]
+
+    callback = SlackStreamingCallbackHandler(channel=channel, ts=ts)
     llm = ChatOpenAI(
         model_name=os.environ["OPENAI_API_MODEL"],
-        temperature=os.environ["OPENAI_API_TEMPERATURE"]
+        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+        streaming=True,
+        callbacks=[callback] 
     )
 
-    response = llm.predict(message)
+    # response = llm.predict(message)
+    llm.predict(message)
 
     # ts=timestamp
-    thread_ts = event["ts"]
+    # thread_ts = event["ts"]
 
-    say(text=response, thread_ts=thread_ts)
+    # say(text=response, thread_ts=thread_ts)
 
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-
-# class SlackStreamingCallbackHandler(BaseCallbackHandler):
-#     last_send_time = time.time()
-#     message = ""
-
-#     def __init__(self, channel, ts):
-#         self.channel = channel
-#         self.ts = ts
-#         self.interval = CHAT_UPDATE_INTERVAL_SEC
-#         # 投稿を更新した累計回数カウンタ
-#         self.update_count = 0
-
-#     def on_llm_new_token(self, token: str, **kwargs) -> None:
-#         self.message += token
-
-#         now = time.time()
-#         if now - self.last_send_time > self.interval:
-#             app.client.chat_update(
-#                 channel=self.channel, ts=self.ts, text=f"{self.message}\n\nTyping..."
-#             )
-#             self.last_send_time = now
-#             self.update_count += 1
-
-#             # update_countが現在の更新間隔X10より多くなるたびに更新間隔を2倍にする
-#             if self.update_count / 10 > self.interval:
-#                 self.interval = self.interval * 2
-
-#     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
-#         message_context = "OpenAI APIで生成される情報は不正確または不適切な場合がありますが、当社の見解を述べるものではありません。"
-#         message_blocks = [
-#             {"type": "section", "text": {"type": "mrkdwn", "text": self.message}},
-#             {"type": "divider"},
-#             {
-#                 "type": "context",
-#                 "elements": [{"type": "mrkdwn", "text": message_context}],
-#             },
-#         ]
-#         app.client.chat_update(
-#             channel=self.channel,
-#             ts=self.ts,
-#             text=self.message,
-#             blocks=message_blocks,
-#         )
 
 # def handle_mention(event, say):
 #     channel = event["channel"]
