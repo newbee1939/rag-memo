47fce30090ba49c25d64582dcbfb8f3ca46cab01
newbee1939 <hideto3970@gmail.com>
2024-02-25 14:59:35 +0900

    update

--- add_vector_data.py
+++ add_vector_data.py
@@ -1,38 +1,34 @@
-# Gitリポジトリから埋め込み表現（Embeddings）を取得してPineconeに保存する
-import logging # ロギング機能を提供するPythonの標準ライブラリ
-import os # オペレーティングシステムとやり取りするための機能を提供するPythonの標準ライブラリ
-
+import logging
+import os
 import pinecone
+import shutil
 from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import GitLoader
-from initialize_vectorstore import initialize_vectorstore
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
 
-# .envファイルから環境変数を読み込む
 load_dotenv()
 
-# ロギング設定を行う。ログのフォーマットとログレベルを指定する
-# https://www.tohoho-web.com/python/logging.html#format
 logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s", 
     level=logging.INFO
 )
-# loggerオブジェクトを作成する
-# 引数はロガー名、__name__はモジュール名
 logger = logging.getLogger(__name__)
 
-def __delete_all_vector_data():
+if __name__ == "__main__":
+    pinecone.init(
+        api_key=os.environ["PINECONE_API_KEY"],
+        environment=os.environ["PINECONE_ENV"],
+    )
+
     index_name = os.environ["PINECONE_INDEX"]
 
     if index_name in pinecone.list_indexes():
+        # 全てのVector Dataを削除
         pinecone.delete_index(index_name)
 
     pinecone.create_index(name=index_name, metric="cosine", dimension=1536)
-
-# GitLoaderで対象のリポジトリを読み込み、CharacterTextSplitterで分割して、Pineconeに保存する
-if __name__ == "__main__":
-    # 全てのVector Dataを全て消す
-    __delete_all_vector_data()
 
     clone_url = "https://github.com/newbee1939/memo"
     branch = "main"
@@ -40,7 +36,7 @@ if __name__ == "__main__":
     filter_ext = ".md"
 
     if os.path.exists(repo_path):
-        clone_url = None
+        shutil.rmtree(repo_path)
 
     loader = GitLoader(
         clone_url=clone_url,
@@ -51,19 +47,18 @@ if __name__ == "__main__":
 
     raw_docs = loader.load()
 
-    # 読み込んだドキュメントの数をログに記録する
+    # 読み込んだドキュメントの数をログに記録
     logger.info("Loaded %d documents", len(raw_docs))
 
-    # CharacterTextSplitterを使用してドキュメントを分割する
+    # ドキュメントを分割
     text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=30)
     docs = text_splitter.split_documents(raw_docs)
 
-    # 分割したドキュメントの数をログに記録する
+    # 分割したドキュメントの数をログに記録
     logger.info("Split %d documents", len(docs))
 
-    # ベクトルストアを初期化する
-    vectorstore = initialize_vectorstore()
+    # 分割されたドキュメントをPineconeに追加
+    embeddings = OpenAIEmbeddings()
+    vectorstore = Pinecone.from_existing_index(index_name, embeddings)
 
-    # 分割されたドキュメントをPineconeに追加する
-    # P212: 重複登録されないために対策が必要
     vectorstore.add_documents(docs)
--- initialize_vectorstore.py
+++ initialize_vectorstore.py
@@ -1,20 +1,14 @@
 import os
-
 import pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 
-# PineconeをLangChainのVectors storeとして使う準備を整える関数
 def initialize_vectorstore():
-    # Pineconeに接続し、APIキーと環境を設定する
     pinecone.init(
         api_key=os.environ["PINECONE_API_KEY"],
         environment=os.environ["PINECONE_ENV"],
     )
 
-    # インデックス名を取得し、OpenAIEmbeddingsを用いてPineconeに既存のインデックスを作成する
     index_name = os.environ["PINECONE_INDEX"]
-    # OpenAI の埋め込み（ベクトル表現）を取得します。これは、テキストや単語などの自然言語処理タスクで使用されるテキストデータの特徴を表現するベクトルです。
-    # 例えば、ある単語が他の単語とどの程度関連しているかを数値化することができます
     embeddings = OpenAIEmbeddings()
     return Pinecone.from_existing_index(index_name, embeddings)
